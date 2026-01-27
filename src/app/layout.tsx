9bcb73b455e2f5c413290e790df290b43beb06c1
Pol <pax@pillyze.com>
2026-01-27 19:46:02 +0900

    오픈그래프 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,22 @@ export const metadata: Metadata = {
   title: "ZROOM - 취미로 연결되는 우리들의 공간",
   description: "아직도 혼자 해? ZROOM에서 너와 딱 맞는 취미 방에 입장해 봐. 지금 가장 핫한 취향들이 실시간으로 모이는 곳!",
   keywords: "ZROOM, 지룸, 취미SNS, 관심사채팅, 덕질공유, 크루모임, 실시간소통",
+  openGraph: {
+    title: "ZROOM - 취미로 연결되는 우리들의 공간",
+    description: "아직도 혼자 해? ZROOM에서 너와 딱 맞는 취미 방에 입장해 봐. 지금 가장 핫한 취향들이 실시간으로 모이는 곳!",
+    type: "website",
+    locale: "ko_KR",
+    siteName: "ZROOM",
+    images: [
+      {
+        url: "https://zroom.io/og_image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "ZROOM",
+      },
+    ],
+    url: "https://zroom.io",
+  },
 };
 
 export default function RootLayout({
