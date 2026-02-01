857bf1fd3f0cace22d94d1976e4272420609c9d2
pol <grapheople@gmail.com>
2026-02-02 01:09:50 +0900

    던전추가

--- src/app/superpet/types.ts
+++ src/app/superpet/types.ts
@@ -187,7 +187,7 @@ export function addExpToCharacter(exp: number): { character: Character; leveledU
 
         // 레벨업 시 스탯 증가
         character.hp += 5;
-        character.attack += 2;
+        character.attack += 1;
         character.defense += 1;
         character.speed += 1;
 
